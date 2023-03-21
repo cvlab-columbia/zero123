798abfe1a99d1bc43ad358dd3e12111819599d9a
Ruoshi Liu <rl3111@columbia.edu>
2023-03-21 12:00:52 -0400

    Update gradio_new.py

--- zero123/gradio_new.py
+++ zero123/gradio_new.py
@@ -53,7 +53,7 @@ _ARTICLE = 'See uses.md'
 
 def load_model_from_config(config, ckpt, device, verbose=False):
     print(f'Loading model from {ckpt}')
-    pl_sd = torch.load(ckpt, map_location=device)
+    pl_sd = torch.load(ckpt, map_location='cpu')
     if 'global_step' in pl_sd:
         print(f'Global Step: {pl_sd["global_step"]}')
     sd = pl_sd['state_dict']
